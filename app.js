cbb7e4b7be7ac3909594624e4c56bd524c3060aa
Vinicius Elias <73002513+Vigonel@users.noreply.github.com>
2025-06-11 16:50:17 -0300

    Alterar range do jogo

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function exibirTextoTela (tag, texto) {
 
 function exibirMensagemInicial () {
     exibirTextoTela ('h1', 'Jogo do Numero Secreto');
-    exibirTextoTela ('p', 'Escolha um número de 1 a 10');
+    exibirTextoTela ('p', 'Escolha um número de 1 a 100');
 }
 
 function verificarChute() {
@@ -85,7 +85,7 @@ function reiniciaJogo() {
 }
 
 let listaNumSorteados = [];
-let numMaxJogo = 10;
+let numMaxJogo = 100;
 let tentativa = 0;
 let numeroSecreto = gerarNumAleatorio ();
 exibirMensagemInicial ();
